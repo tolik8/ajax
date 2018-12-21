e649761081530aa59eb567b6c94d3b7cde53d590
tolik8 <tolik8@gmail.com>
2018-12-21 23:10:28 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
 
 $('.getInfo').click(function() {
-  var login = $('#login').text();
-  var n = (this).id.substr(7);
-  var rnd = Math.random();
 
-  $.post('ajax/get_phone.php', {login:login, n:n, rnd:rnd}, function(data) {
-    $('#tel_'+n).html(data);
-    $('#getTel_'+n).attr('class', 'hide');
-  });
+    var $login = $('#login').text();
+    var $n = (this).id.substr(7);
+    var $random = Math.random();
+    
+    $.ajax({
+        method: "POST",
+        url: "ajax/get_phone.php",
+        data: {login: $login, n: $n, random: $random}
+    }).done(function(msg){
+        $('#tel_' + $n).html(msg);
+        $('#getTel_' + $n).attr('class', 'hide');
+    });
+
 });
 
 });
